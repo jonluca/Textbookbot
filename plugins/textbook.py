3a05a06bd686c837bcc03699405dec2a1e55bb91
JonLuca DeCaro <jdecaro@usc.edu>
2017-10-12 17:11:26 -0700

    Comments

--- plugins/textbook.py
+++ plugins/textbook.py
@@ -1,9 +1,11 @@
 import re
-import googlebooks
+
 import requests
 from bs4 import BeautifulSoup
 from slackbot.bot import listen_to
 from slackbot.bot import respond_to
+
+import plugins.googlebooks as googlebooks
 
 COOKIES = {'lg_topic': 'libgen', }
 
@@ -12,19 +14,21 @@ VALID_URLS = ["libgen.pw/view", "libgen.io/ads", "bookfi.net/md5/", "b-ok.org/md
 api = googlebooks.Api()
 
 
-# NB. Original query string below. It seems impossible to parse and
-# reproduce query strings 100% accurately so the one below is given
-# in case the reproduced version is not "correct".
-# requests.get('http://gen.lib.rus.ec/search.php?req=0321334876&open=0&res=25&view=simple&phrase=0&column=identifier', headers=headers, cookies=cookies)
-
 def find_book(message, isbn):
+	# Params for the request
 	params = (('req', isbn), ('open', '0'), ('res', '25'), ('view', 'simple'), ('phrase', '0'), ('column', 'identifier'),)
 	res = requests.get('http://gen.lib.rus.ec/search.php', headers=HEADERS, params=params, cookies=COOKIES)
+	# instantiate the google books api. Adds overhead but I like when it prints the title, so I'm keeping it
 	book = api.list('isbn:' + isbn)
 	if book["totalItems"] != 0:
-		message.reply("Searching for book " + json.dumps(book))
+		# I don't know how often this fails... Hopefully not too much haha
+		message.reply("Searching for book " + book["items"][0]["volumeInfo"]["title"])
+
+	# Parse HTMl
 	page = BeautifulSoup(res.content, 'html.parser')
 	valid = []
+
+	# Definitely not optimal, but I gave myself 45 minutes to make this, so first thought that pops in my head is what I use haha
 	for link in page.find_all('a', href=True):
 		for URL in VALID_URLS:
 			if link['href'] and URL in link['href']:
@@ -38,6 +42,7 @@ def find_book(message, isbn):
 		message.reply("No links found")
 
 
+# Respond to DMs
 @respond_to('ISBN (.*)', re.IGNORECASE)
 def isbn(message, isbn):
 	isbn = re.sub("[^0-9]", "", isbn)
@@ -46,6 +51,7 @@ def isbn(message, isbn):
 	find_book(message, isbn);
 
 
+# Responds to mentions in channels its in
 @listen_to('textbook ISBN (.*)', re.IGNORECASE)
 def isbn(message, isbn):
 	isbn = re.sub("[^0-9]", "", isbn)
